3c55cabe2fa3dfd0f7bf171bfda62b1fabfc136f
pgswinter <tuananh.nguyen.freelance@gmail.com>
2019-11-27 21:58:49 +0700

    update

--- router/index.js
+++ router/index.js
@@ -1,7 +1,5 @@
-import NasaLib from '../controllers';
-
 export default (app) => {
     app.get('/api', (req, res) => res.status(200).send({
-        message: 'Welcome to the NasaLib API!',
+        message: 'Welcome to the Data API!',
     }));
 }
